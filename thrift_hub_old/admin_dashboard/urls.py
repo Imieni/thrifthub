f2a8cce9b9cd005e6e8eb49922d7b41319c67a57
Imieni Odijie <imieniodijie@gmail.com>
2024-10-21 16:14:18 +0100

    Delete thrift_hub_old directory

--- thrift_hub_old/admin_dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dashboard-admin', views.dashboard_admin, name='dashboard_admin'),
-    path('add-category', views.add_category, name='add_category')
-]
